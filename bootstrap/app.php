3958e561ac8b68064f3ce5d763254c73ab23eb97
noemibnunes <noemibezerra1@gmail.com>
2024-10-12 16:23:49 -0300

    feat: add command para finalizar os projetos que passaram do prazo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
@@ -10,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('app:close-project')->everyMinute();
+    })
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
